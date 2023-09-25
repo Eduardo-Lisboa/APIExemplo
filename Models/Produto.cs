51b1a2428d32f6310eb3e7bc284b9a69914070a8
autologon <autologon@PR04-LAB02-33>
2023-09-25 20:56:47 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIExemplo.Models
+{
+    public class Produto
+    {
+        [Key]
+        [Required]
+        public int IdProduto { get; set; }
+        public string Descricao { get; set; }
+
+        public double Preco { get; set; }
+
+        public int QtdMin { get; set; }
+
+        public string Categoria { get; set; }
+
+        public double Tamanho { get; set; }
+
+        public string Cor { get; set; }
+    }
+}
